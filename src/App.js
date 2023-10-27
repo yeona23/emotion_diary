49882768cac7e7cc034425c21dd089722b51e7e4
yeona23 <136429602+yeona23@users.noreply.github.com>
2023-10-28 04:52:33 +0900

    이미지 추가
    이미지 별도의 컴포넌트로 관리
    폰트 설정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import { getEmotionImgById } from "./util";
 
 function App() {
   return (
     <div className="App">
-      <h1>감정일기장</h1>
+      <img alt="감정1" src={getEmotionImgById(1)} />
+      <img alt="감정2" src={getEmotionImgById(2)} />
+      <img alt="감정3" src={getEmotionImgById(3)} />
+      <img alt="감정4" src={getEmotionImgById(4)} />
+      <img alt="감정5" src={getEmotionImgById(5)} />
     </div>
   );
 }
